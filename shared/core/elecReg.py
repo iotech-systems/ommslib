d417d50afd360d5432614e52eb19cb60404eb741
erik <erik@iotech.systems>
2023-02-09 23:00:39 +0100

    pzem first run fix

--- shared/core/elecReg.py
+++ shared/core/elecReg.py
@@ -1,6 +1,7 @@
 
 import xml.etree.ElementTree as _et
 from ommslib.shared.core.elecRegStrEnums import elecRegStrEnumsShort
+from core.logutils import logUtils
 
 
 """
@@ -9,8 +10,11 @@ from ommslib.shared.core.elecRegStrEnums import elecRegStrEnumsShort
 class elecReg(object):
 
    def __init__(self, xmlelm: _et.Element):
-      self.xmlelm: _et.Element = xmlelm
-      __tmp = self.xmlelm.attrib["type"]
-      self.regtype: elecRegStrEnumsShort = elecRegStrEnumsShort[__tmp]
-      self.default = self.xmlelm.attrib["default"]
-      self.lbl = self.xmlelm.attrib["lbl"]
+      try:
+         self.xmlelm: _et.Element = xmlelm
+         __tmp = self.xmlelm.attrib["type"]
+         self.regtype: elecRegStrEnumsShort = elecRegStrEnumsShort[__tmp]
+         self.default = self.xmlelm.attrib["default"]
+         self.lbl = self.xmlelm.attrib["lbl"]
+      except Exception as e:
+         logUtils.log_exp(["elecReg.c-tor", e])
--- shared/core/elecRegStrEnums.py
+++ shared/core/elecRegStrEnums.py
@@ -3,70 +3,70 @@ from enum import Enum
 
 
 # register names table
-class elecRegStrEnums(Enum):
-
-   ModbusAddress = "ModbusAddress"
-   SerialNumber = "SerialNumber"
-   BaudRate = "BaudRate"
-   HardwareVersion = "HardwareVersion"
-   SoftwareVersion = "SoftwareVersion"
-   CT_Rate = "CT_Rate"
-   S0_OutputRate = "S0_OutputRate"
-   A3 = "A3"
-   CycleTime = "CycleTime"
-   # line(s) frequency ~ Hz
-   GridFreqHz = "GridFreqHz"
-   L1_FreqHz = "L1_FreqHz"
-   L2_FreqHz = "L2_FreqHz"
-   L3_FreqHz = "L3_FreqHz"
-   # voltage ~ V
-   LineVoltage = "LineVoltage"                              # used by 1phase and 3phase edge_meters
-   L1_Voltage = "L1_Voltage"
-   L2_Voltage = "L2_Voltage"
-   L3_Voltage = "L3_Voltage"
-   # amps ~ A
-   TotalAmps = "TotalAmps"                                  # used by 1phase and 3phase edge_meters
-   L1_Amps = "L1_Amps"
-   L2_Amps = "L2_Amps"
-   L3_Amps = "L3_Amps"
-   # active power ~ watts
-   TotalActivePower = "TotalActivePower"                    # used by 1phase and 3phase edge_meters
-   L1_ActivePower = "L1_ActivePower"
-   L2_ActivePower = "L2_ActivePower"
-   L3_ActivePower = "L3_ActivePower"
-   # reactive power ~ Volt-Ampere reactive i.e. VAR
-   TotalReactivePower = "TotalReactivePower"                # used by 1phase and 3phase edge_meters
-   L1_ReactivePower = "L1_ReactivePower"
-   L2_ReactivePower = "L2_ReactivePower"
-   L3_ReactivePower = "L3_ReactivePower"
-   # apparent power ~ units of Volt-amp (VA)
-   TotalApparentPower = "TotalApparentPower"                # used by 1phase and 3phase edge_meters
-   L1_ApparentPower = "L1_ApparentPower"
-   L2_ApparentPower = "L2_ApparentPower"
-   L3_ApparentPower = "L3_ApparentPower"
-   # power factor
-   TotalPowerFactor = "TotalPowerFactor"                    # used by 1phase and 3phase edge_meters
-   L1_PowerFactor = "L1_PowerFactor"
-   L2_PowerFactor = "L2_PowerFactor"
-   L3_PowerFactor = "L3_PowerFactor"
-   # misc
-   CRC = "CRC"
-   CombinedCode = "CombinedCode"
-   # kwh ~ kWh
-   TotalActiveEnergy = "TotalActiveEnergy"                  # used by 1phase and 3phase edge_meters
-   L1_TotalActiveEnergy = "L1_TotalActiveEnergy"
-   L2_TotalActiveEnergy = "L2_TotalActiveEnergy"
-   L3_TotalActiveEnergy = "L3_TotalActiveEnergy"
-   # - - - -
-   TotalReactiveEnergy = "TotalReactiveEnergy"              # used by 1phase and 3phase edge_meters
-   L1_ReactiveEnergy = "L1_ReactiveEnergy"
-   L2_ReactiveEnergy = "L2_ReactiveEnergy"
-   L3_ReactiveEnergy = "L3_ReactiveEnergy"
-   # forward active energy
-   ForwardActiveEnergy = "ForwardActiveEnergy"
-   L1_ForwardActiveEnergy = "L1_ForwardActiveEnergy"
-   L2_ForwardActiveEnergy = "L2_ForwardActiveEnergy"
-   L3_ForwardActiveEnergy = "L3_ForwardActiveEnergy"
+# class elecRegStrEnums(Enum):
+#
+#    ModbusAddress = "ModbusAddress"
+#    SerialNumber = "SerialNumber"
+#    BaudRate = "BaudRate"
+#    HardwareVersion = "HardwareVersion"
+#    SoftwareVersion = "SoftwareVersion"
+#    CT_Rate = "CT_Rate"
+#    S0_OutputRate = "S0_OutputRate"
+#    A3 = "A3"
+#    CycleTime = "CycleTime"
+#    # line(s) frequency ~ Hz
+#    GridFreqHz = "GridFreqHz"
+#    L1_FreqHz = "L1_FreqHz"
+#    L2_FreqHz = "L2_FreqHz"
+#    L3_FreqHz = "L3_FreqHz"
+#    # voltage ~ V
+#    LineVoltage = "LineVoltage"                              # used by 1phase and 3phase edge_meters
+#    L1_Voltage = "L1_Voltage"
+#    L2_Voltage = "L2_Voltage"
+#    L3_Voltage = "L3_Voltage"
+#    # amps ~ A
+#    TotalAmps = "TotalAmps"                                  # used by 1phase and 3phase edge_meters
+#    L1_Amps = "L1_Amps"
+#    L2_Amps = "L2_Amps"
+#    L3_Amps = "L3_Amps"
+#    # active power ~ watts
+#    TotalActivePower = "TotalActivePower"                    # used by 1phase and 3phase edge_meters
+#    L1_ActivePower = "L1_ActivePower"
+#    L2_ActivePower = "L2_ActivePower"
+#    L3_ActivePower = "L3_ActivePower"
+#    # reactive power ~ Volt-Ampere reactive i.e. VAR
+#    TotalReactivePower = "TotalReactivePower"                # used by 1phase and 3phase edge_meters
+#    L1_ReactivePower = "L1_ReactivePower"
+#    L2_ReactivePower = "L2_ReactivePower"
+#    L3_ReactivePower = "L3_ReactivePower"
+#    # apparent power ~ units of Volt-amp (VA)
+#    TotalApparentPower = "TotalApparentPower"                # used by 1phase and 3phase edge_meters
+#    L1_ApparentPower = "L1_ApparentPower"
+#    L2_ApparentPower = "L2_ApparentPower"
+#    L3_ApparentPower = "L3_ApparentPower"
+#    # power factor
+#    TotalPowerFactor = "TotalPowerFactor"                    # used by 1phase and 3phase edge_meters
+#    L1_PowerFactor = "L1_PowerFactor"
+#    L2_PowerFactor = "L2_PowerFactor"
+#    L3_PowerFactor = "L3_PowerFactor"
+#    # misc
+#    CRC = "CRC"
+#    CombinedCode = "CombinedCode"
+#    # kwh ~ kWh
+#    TotalActiveEnergy = "TotalActiveEnergy"                  # used by 1phase and 3phase edge_meters
+#    L1_TotalActiveEnergy = "L1_TotalActiveEnergy"
+#    L2_TotalActiveEnergy = "L2_TotalActiveEnergy"
+#    L3_TotalActiveEnergy = "L3_TotalActiveEnergy"
+#    # - - - -
+#    TotalReactiveEnergy = "TotalReactiveEnergy"              # used by 1phase and 3phase edge_meters
+#    L1_ReactiveEnergy = "L1_ReactiveEnergy"
+#    L2_ReactiveEnergy = "L2_ReactiveEnergy"
+#    L3_ReactiveEnergy = "L3_ReactiveEnergy"
+#    # forward active energy
+#    ForwardActiveEnergy = "ForwardActiveEnergy"
+#    L1_ForwardActiveEnergy = "L1_ForwardActiveEnergy"
+#    L2_ForwardActiveEnergy = "L2_ForwardActiveEnergy"
+#    L3_ForwardActiveEnergy = "L3_ForwardActiveEnergy"
 
 
 class elecRegStrEnumsShort(Enum):
